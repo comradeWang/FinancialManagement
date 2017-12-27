9383c2c3c5a150728b5e3d1f0d61d87c2f8fe0d4
WangXinYu <615948603@qq.com>
2017-12-27 20:26:58 +0800

    删除原来项目

--- src/main/java/com/team/finance/login/service/loginService.java
+++
@@ -1,9 +0,0 @@
-package com.team.finance.login.service;
-
-/**
- * @author: Avenger
- * @describe: TODO
- * @create: 2017年12月19日 8:46
- **/
-public interface loginService {
-}
--- src/main/java/com/team/finance/login/service/serviceImpl/loginServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.team.finance.login.service.serviceImpl;
-
-/**
- * @author: Avenger
- * @describe: TODO
- * @create: 2017年12月19日 8:48
- **/
-public class loginServiceImpl {
-}
